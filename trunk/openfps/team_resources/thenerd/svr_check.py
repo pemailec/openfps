8290e694910db051c8f3fe3fa403522be8d6d318
chrisdalke@gmail.com <chrisdalke@gmail.com@e4cf71a6-0f48-11df-b535-e1565790bc13>
2011-02-13 01:30:13 +0000

    -Rearranged the model directory as well as added new models -Worked on just about everything

--- trunk/openfps/team_resources/thenerd/svr_check.py
+++ trunk/openfps/team_resources/thenerd/svr_check.py
@@ -1,3 +1,7 @@
+print "////////////////////////////////////////"
+print "OPENFPS UPDATE CHECKER"
+print "////////////////////////////////////////"
+
 print "Connecting..."
 import socket
 import os
@@ -8,19 +12,17 @@ url = urllib.URLopener()
 resp = url.open('https://openfps.googlecode.com/svn/trunk/openfps/')
 print "Server https://openfps.googlecode.com/svn/trunk/openfps/"
 
-
+print "////////////////////////////////////////"
+print "FileList:"
 
 html = resp.read(114)
 
 end = html.find("</title>")
 start = html.find("<title>")+7
       
-for i in range(20):
-    print i*5+5,"%",
-    time.sleep(0.5)
-    print "\b\b\b\b"
+time.sleep(0.5)
 
-print " ... Checkout complete."
+
 print html[start:end].strip()
 
 for dirname, dirnames, filenames in os.walk('.'):
@@ -28,6 +30,6 @@ for dirname, dirnames, filenames in os.walk('.'):
         print os.path.join(dirname, subdirname)
     for filename in filenames:
         print os.path.join(dirname, filename)
-
-
+print "////////////////////////////////////////"
+print " ... Checkout test complete."
 raw_input("enter key to exit...")
